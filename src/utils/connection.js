6b487f72def95c8b567273a05961e5008d010daf
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2024-12-02 18:28:26 -0400

    Update connection.js

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL_DEV, {logging: false})
+const sequelize = new Sequelize(process.env.DATABASE_URL_PROD, {logging: false})
 
 module.exports = sequelize;
