cc0e4e6d8a64c85eb717171177ef4261677ba3a2
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-01-28 08:16:23 -0400

    Update isAdmin.middleware.js

--- src/middlewares/isAdmin.middleware.js
+++ src/middlewares/isAdmin.middleware.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const isAdmin = (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
-    const token = authHeader.split(' ')[1]; 
+    const authHeader = req.headers?.authorization || req.headers?.Authorization;
+    // if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+    const token = authHeader?.split(' ')[1]; 
     try {
         const { user } = jwt.verify(token, process.env.TOKEN_SECRET);
         // if (user.roleId !== 1 || !user.status) {
